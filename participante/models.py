6af3e73699b135ef09566db5e059a69be659c681
pc-info <t-vitor@iabs.org.br>
2018-11-26 09:23:29 -0200

    Formulario model

--- participante/models.py
+++ participante/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+class Formulario(models.Model):
+
+    participante = models.ForeignKey(
+        Participante,
+        on_delete=models.DO_NOTHING)
+
+    posto_cadastramento = models.ForeignKey(
+        PostoCadastramento,
+        on_delete=models.DO_NOTHING)
+
+    numero_cartao = models.BigIntegerField()
+
+    lote = models.IntegerField()
+
+    sublote = models.IntegerField()
+
+    data_recebimento_cartao = models.DateTimeField(
+        blank=True, null=True)
+
+    data_inclusao = models.DateTimeField()
+
+    data_alteracao = models.DateTimeField(
+        blank=True, null=True)
+
+    observacao = models.TextField(
+        blank=True, null=True)
+
+    data_cartao_inicializado = models.DateTimeField(
+        blank=True, null=True)
+
+    nisrlfamilia = models.CharField(
+        max_length=15, blank=True, null=True)
+
+    nisrlfamiliadtref = models.DateTimeField(
+        blank=True, null=True)
+
+    nisrlfamiliadtatual = models.DateTimeField(
+        blank=True, null=True)
+
+
+
+
+
